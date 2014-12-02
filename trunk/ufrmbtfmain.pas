da1486f2b75b264b379565985c0cd3708fa1509a
sx.slex@gmail.com <sx.slex@gmail.com>
2014-12-02 16:41:25 -0200

    Simple program designed to put a window always in front on windows operating system

--- trunk/ufrmbtfmain.pas
+++ trunk/ufrmbtfmain.pas
@@ -1,3 +1,7 @@
+(*
+Simple program designed to put a window in operaciona windows system always in
+front.
+*)
 unit ufrmBTFMain;
 
 {$mode objfpc}{$H+}
@@ -5,19 +9,24 @@ unit ufrmBTFMain;
 interface
 
 uses
-  JwaTlHelp32,
+  Windows,
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
   Buttons;
 
 type
-
+//  HWND = type LongWord;
+//  BOOL = LongBool;
   { Tfrmbtfmain }
 
   Tfrmbtfmain = class(TForm)
     btnSetToRight: TButton;
     btnSetNoTopMost: TButton;
-    ComboBox1: TComboBox;
+    cmbWindows: TComboBox;
     btnRefreshWindows: TSpeedButton;
+    procedure btnRefreshWindowsClick(Sender: TObject);
+    procedure btnSetNoTopMostClick(Sender: TObject);
+    procedure btnSetToRightClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
   private
     { private declarations }
   public
@@ -31,20 +40,55 @@ implementation
 
 {$R *.lfm}
 
-function EnumWindowsProc3(Wnd: THandle; lb: TListbox): Boolean; stdcall;
+function EnumWindowsProc(Wnd: HWND; LParM: LParam): LongBool; stdCall; export;
 var
-    caption: Array [0..128] of Char;
+  caption: Array [0..128] of Char;
 begin
-    Result := True;
-    if IsWindowVisible(Wnd) and
-    ((GetWindowLong(Wnd, GWL_HWNDPARENT) = 0) or
-    (HWND(GetWindowLong(Wnd, GWL_HWNDPARENT)) = GetDesktopWindow))and
-    ((GetWindowLong(Wnd, GWL_EXSTYLE) and WS_EX_TOOLWINDOW) = 0)
-    then
-    begin
-        SendMessage( Wnd, WM_GETTEXT, Sizeof(caption), Integer(@caption));
-        frmUpToFront.ComboBox1.Items.AddObject( caption, TObject( Wnd ));
-    end;
+  Result := True;
+  if IsWindowVisible(Wnd)
+  and (
+      (GetWindowLong(Wnd, GWL_HWNDPARENT) = 0)
+      or  (HWND(GetWindowLong(Wnd, GWL_HWNDPARENT)) = GetDesktopWindow)
+  )
+  and (
+      (GetWindowLong(Wnd, GWL_EXSTYLE) and WS_EX_TOOLWINDOW) = 0
+  )
+  then
+  begin
+    SendMessage( Wnd, WM_GETTEXT, Sizeof(caption), Integer(@caption));
+    frmbtfmain.cmbWindows.Items.AddObject( caption, TObject( Wnd ));
+  end;
+end;
+
+{ Tfrmbtfmain }
+
+procedure Tfrmbtfmain.btnRefreshWindowsClick(Sender: TObject);
+begin
+  cmbWindows.Items.Clear;
+  EnumWindows(@EnumWindowsProc, 0);
+end;
+
+procedure Tfrmbtfmain.btnSetNoTopMostClick(Sender: TObject);
+var
+  hWnd: LongWord;
+begin
+  hWnd := FindWindow(nil, PChar(cmbWindows.Text));
+  SetWindowPos(hWnd, HWND_NOTOPMOST, 0,0,0,0, SWP_NOMOVE+SWP_NOSIZE);
+  SetForegroundWindow(hWnd);
+end;
+
+procedure Tfrmbtfmain.btnSetToRightClick(Sender: TObject);
+var
+  hWnd: LongWord;
+begin
+  hWnd := FindWindow(nil, PChar(cmbWindows.Text));
+  SetWindowPos(hWnd, HWND_TOPMOST, 0,0,0,0, SWP_NOMOVE+SWP_NOSIZE);
+  SetForegroundWindow(hWnd);
+end;
+
+procedure Tfrmbtfmain.FormCreate(Sender: TObject);
+begin
+  btnRefreshWindowsClick(btnRefreshWindows);
 end;
 
 end.
